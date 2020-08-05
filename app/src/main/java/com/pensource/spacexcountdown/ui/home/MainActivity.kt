6aa0acdac340ffb18de728dc42d5707cb55d443f
haseebcgt <abdul.haseeb@cgtindia.in>
2020-08-05 13:23:54 +0530

    Loading background image with glide

--- app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
+++ app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
@@ -3,6 +3,8 @@ package com.pensource.spacexcountdown.ui.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.pensource.spacexcountdown.R
 import com.pensource.spacexcountdown.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity() {
             lifecycleOwner = this@MainActivity
         }
         setContentView(binding.root)
+
+        Glide.with(this).load(R.drawable.placeholder).into(binding.backgroundImage)
 
         viewModel.nextLaunch.observe(this, Observer {})
 
