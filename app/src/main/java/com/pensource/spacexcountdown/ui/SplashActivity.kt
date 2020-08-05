2a6230b38b97ece53fad438dab1013257e37f8bb
haseebcgt <abdul.haseeb@cgtindia.in>
2020-08-06 01:05:00 +0530

    Added splash screen

--- app/src/main/java/com/pensource/spacexcountdown/ui/SplashActivity.kt
+++ app/src/main/java/com/pensource/spacexcountdown/ui/SplashActivity.kt
@@ -1,12 +1,15 @@
 package com.pensource.spacexcountdown.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.pensource.spacexcountdown.R
+import androidx.appcompat.app.AppCompatActivity
+import com.pensource.spacexcountdown.ui.home.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
