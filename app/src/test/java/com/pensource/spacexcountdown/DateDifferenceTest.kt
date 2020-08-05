72c99c9dd8c5f1e53ee592c09919a385805d9b8a
haseebcgt <abdul.haseeb@cgtindia.in>
2020-08-05 12:12:55 +0530

    Added comment in test

--- app/src/test/java/com/pensource/spacexcountdown/DateDifferenceTest.kt
+++ app/src/test/java/com/pensource/spacexcountdown/DateDifferenceTest.kt
@@ -1,5 +1,6 @@
 package com.pensource.spacexcountdown
 
+import com.pensource.spacexcountdown.util.API_DATE_FORMAT
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -15,23 +16,29 @@ class DateDifferenceTest {
 
     @Test
     fun `hour difference test`() {
+        // Given
         val date1 = SimpleDateFormat("HH:mm:ss dd-MM-yyyy").parse("01:00:00 01-01-2000")
         val date2 = SimpleDateFormat("HH:mm:ss dd-MM-yyyy").parse("02:00:00 01-01-2000")
 
+        // When
         val diff = date2.time - date1.time
         val expectedDiff: Long = 1000 * 60 * 60
 
+        // Then
         assertEquals(expectedDiff, diff)
     }
 
     @Test
     fun `day difference test`() {
+        // Given
         val date1 = SimpleDateFormat("HH:mm:ss dd-MM-yyyy").parse("01:00:00 01-01-2000")
         val date2 = SimpleDateFormat("HH:mm:ss dd-MM-yyyy").parse("01:00:00 02-01-2000")
 
+        // When
         val diff = date2.time - date1.time
         val expectedDiff: Long = 1000 * 60 * 60 * 24
 
+        // Then
         assertEquals(expectedDiff, diff)
     }
 }
