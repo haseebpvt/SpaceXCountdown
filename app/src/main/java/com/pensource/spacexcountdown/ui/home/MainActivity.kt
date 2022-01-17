fb9cf8339f261d04d607af14783747d2208c3abd
Haseeb <haseebpvt@gmail.com>
2022-01-17 23:42:38 +0530

    Added sample compose view in existing layout

--- app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
+++ app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
@@ -3,12 +3,25 @@ package com.pensource.spacexcountdown.ui.home
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.pensource.spacexcountdown.R
 import com.pensource.spacexcountdown.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,5 +45,34 @@ class MainActivity : AppCompatActivity() {
         viewModel.nextLaunchCountdown.observe(this, Observer {
             binding.countDownTimer.start(it)
         })
+
+        findViewById<ComposeView>(R.id.compose_view).setContent {
+            MaterialTheme {
+                TimeCard("SPACEX COUNTDOWN")
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeCard(time: String) {
+    Card(backgroundColor = Color.Black) {
+        Column {
+            Image(painterResource(R.drawable.ic_logo_only_x), "Falcon")
+            Text(
+                time,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center,
+                color = Color.Red
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        TimeCard("SPACEX COUNTDOWN")
     }
 }
