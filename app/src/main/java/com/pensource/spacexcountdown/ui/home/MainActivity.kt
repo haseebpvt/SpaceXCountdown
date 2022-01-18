54452e6df03f43655c660bdc027fe910dcd43b79
Haseeb <haseebpvt@gmail.com>
2022-01-18 22:56:01 +0530

    Removed content xml and replaced it with compose view

--- app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
+++ app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pensource.spacexcountdown.ui.home
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
@@ -28,27 +29,11 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: HomeViewModel by viewModels()
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            viewmodel = viewModel
-            lifecycleOwner = this@MainActivity
-        }
-        setContentView(binding.root)
-
-        Glide.with(this).load(R.drawable.falcon_nine_animation).into(binding.animation)
-
-        viewModel.nextLaunch.observe(this, Observer {})
-
-        viewModel.nextLaunchCountdown.observe(this, Observer {
-            binding.countDownTimer.start(it)
-        })
-
-        findViewById<ComposeView>(R.id.compose_view).setContent {
+        setContent {
             MaterialTheme {
-                TimeCard("SPACEX COUNTDOWN")
+                TimeCard("Hello")
             }
         }
     }
