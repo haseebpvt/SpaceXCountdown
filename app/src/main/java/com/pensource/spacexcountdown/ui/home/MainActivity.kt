27edf61b8e10bc01b8d875559a1f4be558253c10
Haseeb <haseebpvt@gmail.com>
2022-01-19 23:31:16 +0530

    Code cleaned

--- app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
+++ app/src/main/java/com/pensource/spacexcountdown/ui/home/MainActivity.kt
@@ -13,15 +13,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Observer
-import com.bumptech.glide.Glide
 import com.pensource.spacexcountdown.R
-import com.pensource.spacexcountdown.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
